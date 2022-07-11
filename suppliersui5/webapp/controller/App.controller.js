d8cccd76f8d69d230338b9764e0be0701aab7247
Mat-Byte <mateocano_c@hotmail.com>
2022-07-11 01:01:39 +0000

    10072022

---
+++ suppliersui5/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("mcc.suppliersui5.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
